3d51ecb5a29f0d259b09e1c0560e4113f1459ccf
Derek Babb <dbabb1@gmail.com>
2020-01-26 22:17:14 -0600

    Template file for WeatherApp project.

---
+++ WeatherApp.py
@@ -0,0 +1,26 @@
+#WeatherApp.py
+#Name:
+#Date:
+#Assignment:
+
+import WeatherInfo
+
+#Set your key
+WeatherInfo.setKey("<INSERT YOUR KEY HERE>")
+
+#Ask the user for their city
+
+#Update the weather with the given city
+
+#Request any data you need from the WeatherInfo API
+
+#Process the data
+#convert temperature to fahrenheit,
+#determine wind speed in words
+#decide jacket and umbrella status
+
+#Report to the user the weather of their city
+
+#Ask user if they would like another weather report
+#If yes, loop to the top of your program where they are asked for a city.
+#If no, end with a goodbye statement of some sort.
